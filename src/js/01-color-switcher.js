7edea492c23f0c598af55c0738ed3f8c944a4877
win0rdie <rostyslavhnatovskyi@gmail.com>
2022-11-08 23:13:14 +0200

    9-1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyEl = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function disableBtn() {
+  startBtn.disabled = true;
+}
+
+function enableBtn() {
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', () => {
+  interval = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  disableBtn();
+});
+
+stopBtn.addEventListener('click', () => {
+  enableBtn();
+  clearInterval(interval);
+});
