135ccbaadf91b923ff691016649348cbfc60aa80
win0rdie <rostyslavhnatovskyi@gmail.com>
2022-11-11 00:28:43 +0200

    9-3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const btnEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+  return promise;
+}
+
+function handleClickBtn(e) {
+  e.preventDefault();
+  let delay = +e.currentTarget.delay.value;
+  let step = +e.currentTarget.step.value;
+  let amount = +e.currentTarget.amount.value;
+
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    delay += step;
   }
 }
+
+btnEl.addEventListener('submit', handleClickBtn);
